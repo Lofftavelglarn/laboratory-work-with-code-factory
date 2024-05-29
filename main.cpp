45f397dd06ad1387865faa3360569f43a42c0cd0
Lofftavelglarn <makar.gorohowik@yandex.ru>
2024-05-29 14:07:30 +0700

    В main добавлена проверка на отстутствие фабрики

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@
 #include "csconcretecodefactory.h"
 #include "javaconcretecodefactory.h"
 #include <iostream>
+#include <QObject>
 
 std::string generateProgram(std::shared_ptr<AbstractCodeFactory>& factory){
+    if(!factory){
+        qWarning("The factory has not been selected. Factory will be set to default (cpp)");
+        factory = std::make_shared<CppConcreteCodeFactory>();
+    }
     std::shared_ptr<AbstractClassUnit> myClass = factory->createClass("MyClass");
     myClass->add(
         factory->createMethod("testFunc1", "void", 0),
@@ -35,6 +40,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     std::shared_ptr<AbstractCodeFactory> factory = std::make_shared<CppConcreteCodeFactory>();
+    factory = nullptr;
     std::cout << generateProgram(factory) << std::endl;
 
     return a.exec();
